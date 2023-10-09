255b0eb98702b6ed8b1a9a2e4ddf81adbb2112a5
Malay Koladiya <malaykoladiya@gmail.com>
2023-10-08 17:16:50 -0700

    added to_json() method

--- backend/models/employer.py
+++ backend/models/employer.py
@@ -1,26 +1,34 @@
-from flask import jsonify, request, session, redirect
-from flask_login import logout_user, login_required, login_user, UserMixin
-
 from flask_login import UserMixin
 
 class Employer(UserMixin):
     def __init__(self, em_data=None):
         if em_data:
-            self.id = str(em_data.get('_id'))
+            self._id = str(em_data.get('_id'))
             self.name = em_data.get('name')
             self.email = em_data.get('email')
             self.password = em_data.get('password')
 
+    def to_json(self):
+        """
+        Returns a JSON representation of the object
+        """
+        return {
+            '_id': self._id,
+            'name': self.name,
+            'email': self.email,
+            'password': self.password
+        }
+    
     def is_employer(self):
         return True
     
-    def em_get_id(self):
-        return self.id  # Return a unique identifier for the user
+    def get_id(self):
+        return str(self._id)  # Return a unique identifier for the user
 
     @classmethod
     def em_get_by_id(cls, em_id, db):
         # Implement a method to retrieve a user by their ID from the database
-        return Employer(db.user.find_one({'_id': em_id}))
+        return Employer(db.employer.find_one({'_id': em_id}))
     #   Implement this according to your database structure
 
     # You can add more methods as needed for user management
