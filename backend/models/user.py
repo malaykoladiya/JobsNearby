d1e7ad7ccbfedca908e10c5054720faa6b844697
Malay Koladiya <malaykoladiya@gmail.com>
2023-10-08 17:18:55 -0700

    added to_json() method

--- backend/models/user.py
+++ backend/models/user.py
@@ -1,12 +1,9 @@
-from flask import jsonify, request, session, redirect
-from flask_login import logout_user, login_required, login_user, UserMixin
-
 from flask_login import UserMixin
 
 class User(UserMixin):
     def __init__(self, user_data=None):
         if user_data:
-            self.id = str(user_data.get('_id'))
+            self._id = str(user_data.get('_id'))
             self.name = user_data.get('name')
             self.email = user_data.get('email')
             self.password = user_data.get('password')
@@ -16,11 +13,21 @@ class User(UserMixin):
             self.education = user_data.get('education', [])
 
 
+    def to_json(self):
+        """
+        Returns a JSON representation of the object
+        """
+        return {
+            '_id': self._id,
+            'name': self.name,
+            'email': self.email,
+            'password': self.password
+        }
     def is_employer(self):
         return False
     
     def get_id(self):
-        return self.id  # Return a unique identifier for the user
+        return str(self._id)  # Return a unique identifier for the user
 
     @classmethod
     def get_by_id(cls, user_id, db):
